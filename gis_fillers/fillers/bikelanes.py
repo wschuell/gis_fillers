92fb93a8affa6ee38e6953b2d2297d57b2535a94
William Schueller <william.schueller@gmail.com>
2022-07-11 17:04:01 +0200

    update

--- gis_fillers/fillers/bikelanes.py
+++ gis_fillers/fillers/bikelanes.py
@@ -128,10 +128,10 @@ class RoadLengthFiller(fillers.Filler):
 				INNER JOIN zone_levels zl
 				ON zl.name=%(zone_level)s
 				INNER JOIN zones z
-				ON z.level=zl.id
-					INNER JOIN zone_parents zp
-					ON child=z.id AND child_level=z.level
-					AND parent=9 AND parent_level=(SELECT id FROM zone_levels WHERE name='bundesland')
+				ON z.level=zl.id AND z.id=9
+					--INNER JOIN zone_parents zp
+					--ON child=z.id AND child_level=z.level
+					--AND parent=9 AND parent_level=(SELECT id FROM zone_levels WHERE name='bundesland')
 				INNER JOIN gis_types gt
 				ON gt.name=%(gis_type)s
 				INNER JOIN gis_data gd
